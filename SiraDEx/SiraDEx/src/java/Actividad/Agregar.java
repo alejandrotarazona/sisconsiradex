2110989babc0b97e1eede82c53c9c3badd7af4a6
jorgedgb@gmail.com <jorgedgb@gmail.com@c823e776-027a-fdf1-4799-f78f589831ee>
2013-01-30 15:02:51 +0000

    cambios en paquete actividad y Actividad.java

---
+++ SiraDEx/SiraDEx/src/java/Actividad/Agregar.java
@@ -0,0 +1,89 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Actividad;
+
+import Clases.Actividad;
+import Clases.CampoValor;
+import Clases.Usuario;
+import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.DispatchAction;
+
+/**
+ *
+ * @author SisCon
+ */
+public class Agregar extends DispatchAction {
+
+    /*
+     * forward name="success" path=""
+     */
+    private static final String SUCCESS = "success";
+    private static final String SUCCESSFULL = "successfull";
+    private static final String FAILURE = "failure";
+    private static final String PAGE = "page";
+
+    /**
+     * This is the action called from the Struts framework.
+     *
+     * @param mapping The ActionMapping used to select this instance.
+     * @param form The optional ActionForm bean for this request.
+     * @param request The HTTP Request we are processing.
+     * @param response The HTTP Response we are processing.
+     * @throws java.lang.Exception
+     * @return
+     */
+       
+    public ActionForward page(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        ArrayList ta = Clases.TipoActividad.listarTiposActividad();
+        request.setAttribute("tipos", ta);
+        Actividad a = (Actividad) form;
+        a.setMensaje("");
+        return mapping.findForward(PAGE);
+    }
+    
+    public ActionForward save(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+
+        Actividad a = (Actividad) form;
+        
+        ArrayList<CampoValor> valores = a.listarCampos();
+        a.setCamposValores(valores);
+
+        Usuario u = (Usuario) request.getSession().getAttribute("user");
+        String username = u.getUsername();
+        a.setCreador(username);
+        
+        return mapping.findForward(SUCCESS);
+
+    }
+
+    public ActionForward save2(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+
+        Actividad a = (Actividad) form;
+
+
+        if (a.agregarActividad()) {
+
+            a.setMensaje("Su actividad ha sido registrado con éxito.");
+            return mapping.findForward(SUCCESSFULL);
+        }
+
+        a.setMensaje("No se pudo registrar la actividad. Por favor revise que "
+                + "los campos se han llenado correctamente.");
+        return mapping.findForward(FAILURE);
+
+
+    }
+}
