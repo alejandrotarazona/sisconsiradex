f11633d807e178b95dbe5bced160f497814d1c36
alejandrotarazona@gmail.com <alejandrotarazona@gmail.com@c823e776-027a-fdf1-4799-f78f589831ee>
2013-01-31 01:48:49 +0000

    Agregar ElementoCatalogo. Trabajando en listar y consultas.

--- trunk/SiraDEx/SiraDEx/src/java/Clases/ElementoCatalogo.java
+++ trunk/SiraDEx/SiraDEx/src/java/Clases/ElementoCatalogo.java
@@ -4,7 +4,9 @@
  */
 package Clases;
 
+import DBMS.Entity;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  *
@@ -12,27 +14,20 @@ import java.util.ArrayList;
  */
 public class ElementoCatalogo {
     private int                             idCatalogo;
-    private String                          nombreCatalogo;
+    private int                             idElemento;
     private ArrayList<CampoCatalogoValor>   valor;
 
 
     public ElementoCatalogo() {
     }
 
-    public ElementoCatalogo(String nombreCatalogo) {
-        this.nombreCatalogo = nombreCatalogo;
-    }
-
-    public String getNombreCatalogo() {
-        return nombreCatalogo;
-    }
-
-    public void setNombreCatalogo(String nombreCatalogo) {
-        this.nombreCatalogo = nombreCatalogo;
-    }
-
     public int getIdCatalogo() {
         return idCatalogo;
+    }
+    
+    public void setIdElemento(){
+        Entity eElemento = new Entity(0,10);
+        this.idElemento = eElemento.seleccionarMaxId("id_elemento");
     }
 
     public void setIdCatalogo(int idCatalogo) {
@@ -47,5 +42,28 @@ public class ElementoCatalogo {
         this.valor = valor;
     }
   
-    
+    public boolean agregar(){
+        Entity eElemento = new Entity(1,10);
+        boolean resp = true;
+        
+        String[] columnas = {
+            "id_catalogo"
+        };
+        Integer idCat = new Integer(this.idCatalogo);
+        Object[] valores = {
+            idCat
+        };
+        resp &= eElemento.insertar2(columnas, valores);
+        if(resp) {setIdElemento();}
+        else {return resp;}
+        
+        Iterator itValores = this.valor.iterator();
+        
+        while(itValores.hasNext() && resp){
+            CampoCatalogoValor ccv = (CampoCatalogoValor) itValores.next();
+            resp &= ccv.agregar();                
+        }
+        
+        return resp;        
+    }
 }
