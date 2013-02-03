03badb15f76ad09f0d1524c840ce14d53d34cb68
jorgedgb@gmail.com <jorgedgb@gmail.com@c823e776-027a-fdf1-4799-f78f589831ee>
2013-02-03 00:55:51 +0000

    correccion ElementoCatalogo.java

--- SiraDEx/SiraDEx/src/java/Clases/ElementoCatalogo.java
+++ SiraDEx/SiraDEx/src/java/Clases/ElementoCatalogo.java
@@ -5,24 +5,36 @@
 package Clases;
 
 import DBMS.Entity;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author SisCon
  */
 public class ElementoCatalogo {
-    private int                             idCatalogo;
-    private int                             idElemento;
+    private int     idElemento;
+    private int     idCatalogo;
+    private String  catalogo;   
     private ArrayList<CampoCatalogoValor>   valor;
 
-
+private static String[] ATRIBUTOS = {
+        "id_elemento", //1
+        "id_catalogo" //0
+    };
     public ElementoCatalogo() {
     }
 
     public int getIdCatalogo() {
         return idCatalogo;
+    }
+
+    public void setIdElemento(int idElemento) {
+        this.idElemento = idElemento;
     }
     
     public void setIdElemento(){
@@ -41,6 +53,15 @@ public class ElementoCatalogo {
     public void setValor(ArrayList<CampoCatalogoValor> valor) {
         this.valor = valor;
     }
+
+    public String getCatalogo() {
+        return catalogo;
+    }
+
+    public void setCatalogo(String catalogo) {
+        this.catalogo = catalogo;
+    }
+    
   
     public boolean agregar(){
         Entity eElemento = new Entity(1,10);
@@ -71,4 +92,40 @@ public class ElementoCatalogo {
         Entity e = new Entity(5, 10);
         return e.borrar("idElemento", idElemento);
     }
+    
+    public static ArrayList<ElementoCatalogo> listarElementos() {
+        ArrayList<ElementoCatalogo> listaElementoCatalogo = new ArrayList<>(0);
+        Entity eElementoCatalogo = new Entity(0, 10);
+
+        ResultSet rs = eElementoCatalogo.listar();
+
+        Entity eCatalogo = new Entity(0, 8);
+
+        if (rs != null) {
+            try {
+                while (rs.next()) {
+                    ElementoCatalogo ec = new ElementoCatalogo();
+
+                    ec.setIdElemento(rs.getInt(ElementoCatalogo.ATRIBUTOS[0]));
+
+                    int id = rs.getInt(ElementoCatalogo.ATRIBUTOS[1]);
+                    ec.setIdCatalogo(id);
+
+                    String[] nombreCat = {"nombre"};
+                    String[] idCat = {"id_cat"};
+                    Integer[] idAct = {id};
+                    ResultSet r = eCatalogo.proyectar(nombreCat, idCat, idAct);
+                    r.next();
+                    ec.setCatalogo(r.getString(1));
+
+                    listaElementoCatalogo.add(ec);
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(ElementoCatalogo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        return listaElementoCatalogo;
+    }
+
 }
