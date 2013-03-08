c3736ae7dc8366afce212503134f2f3243cd628c
jorgedgb@gmail.com <jorgedgb@gmail.com@c823e776-027a-fdf1-4799-f78f589831ee>
2013-03-08 03:36:07 +0000

    correccion y cambios para verificar los campos fijos cuando se agrega un TipoActividad.

--- SiraDEx/SiraDEx/src/java/TiposActividad/Agregar.java
+++ SiraDEx/SiraDEx/src/java/TiposActividad/Agregar.java
@@ -44,8 +44,8 @@ public class Agregar extends DispatchAction {
     public ActionForward page(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-        TipoActividad t = (TipoActividad) form;
-        t.deleteSessions(request);
+        TipoActividad ta = (TipoActividad) form;
+        ta.deleteSessions(request);
         ArrayList<Elemento> programas;
         programas = Clases.Elemento.listarElementos("Programas", 1);
         request.getSession().setAttribute("programas", programas);
@@ -54,99 +54,77 @@ public class Agregar extends DispatchAction {
         request.getSession().setAttribute("coordinaciones", coordinaciones);
         return mapping.findForward(PAGE);
     }
-    
+
     public ActionForward save(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-        
-        TipoActividad t = (TipoActividad) form;
+
+        TipoActividad ta = (TipoActividad) form;
+        ta.setMensaje(null);
         ArrayList catalogos = Clases.Catalogo.listar();
         request.getSession().setAttribute("catalogos", catalogos);
+
+        if (!Verificaciones.verif(ta)) {
+            return mapping.findForward(FAILURE);
+        }
         
-        if (t.getNombreTipo().contains(";") || t.getNombreTipo().contains("<")
-                || t.getNombreTipo().contains(">") || t.getNombreTipo().contains("'")
-                || t.getNombreTipo().contains("&") || t.getNombreTipo().contains("$")) {
-            t.setMensaje("Error: El nombre del tipo de actividad contiene uno de los "
-                    + "siguientes carácteres inválidos: ;>?'&$ por favor "
-                    + "intente de nuevo.");
-            return mapping.findForward(FAILURE);
-            
-        } else if (t.getNombreTipo().length() > 140) {
-            t.setMensaje("El nombre del tipo de actividad es muy largo");
-            return mapping.findForward(FAILURE);
-        } else if (t.getDescripcion().length() > 2000) {
-            t.setMensaje("La descripcion del tipo de actividad es muy larga");
-            return mapping.findForward(FAILURE);
-        } else if (t.getProducto().length() > 50) {
-            t.setMensaje("El nombre del campo \"Producto\" es muy largo");
-            return mapping.findForward(FAILURE);
-        }
-        else if (t.getNroCampos() <= 0) {
-            t.setMensaje("Error: El número de campos de un tipo de actividad "
-                    + "debe ser de al menos 1. Ingrese un número válido y "
-                    + "presione Siguiente.");
-            return mapping.findForward(FAILURE);
-        } else if (!Verificaciones.verif(t)) {
-            t.setMensaje("Error: Los campos obligatorios (*) deben ser llenados");
-            return mapping.findForward(FAILURE);
-        }
-        int numeroCampos = t.getNroCampos();
+        int numeroCampos = ta.getNroCampos();
         ArrayList<Campo> campos = new ArrayList<>();
-        
+
         for (int i = 0; i < numeroCampos; i++) {
             Campo c = new Campo();
-            c.setIdTipoActividad(t.getId());
+            c.setIdTipoActividad(ta.getId());
             campos.add(c);
         }
-        
-        t.setCampos(campos);
-        
+
+        ta.setCampos(campos);
+
         return mapping.findForward(SUCCESS);
-        
+
     }
-    
+
     public ActionForward save2(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-        
-        TipoActividad t = (TipoActividad) form;
-        t.setMensaje(null);
-        ArrayList<Campo> c = t.getCampos();
-        
-        for (int i = 1; i <= t.getNroCampos(); i++) {
+
+        TipoActividad ta = (TipoActividad) form;
+        ta.setMensaje(null);
+        ArrayList<Campo> c = ta.getCampos();
+
+        for (int i = 1; i <= ta.getNroCampos(); i++) {
             try {
                 Campo aux = c.get(i - 1);
                 if (aux.isNombreInvalido()) {
-                    t.setMensaje("El campo número " + i
+                    ta.setMensaje("El campo número " + i
                             + " contiene un nombre inválido");
                     return mapping.findForward(FAILURE2);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
-                t.setMensaje("El campo número " + i
+                ta.setMensaje("El campo número " + i
                         + " contiene un nombre inválido");
                 return mapping.findForward(FAILURE2);
             }
         }
-        
-        if (Verificaciones.verif(t.getCampos())) {
-            
-            if (t.agregarTipoActividad()) {
-                
-                t.setMensaje("El tipo de actividad '" + t.getNombreTipo() + "' ha sido "
+
+        if (Verificaciones.verif(ta.getCampos())) {
+
+            if (ta.agregarTipoActividad()) {
+
+                ta.setMensaje("El tipo de actividad '" + ta.getNombreTipo() + "' ha sido "
                         + "registrado con éxito.");
-                ArrayList ta = Clases.TipoActividad.listar();
-                request.setAttribute("tipos", ta);
+                ArrayList tipos = Clases.TipoActividad.listar();
+                request.setAttribute("tipos", tipos);
                 return mapping.findForward(SUCCESSFULL);
             }
             return mapping.findForward(FAILURE);
         } else {
-            t.setMensaje("Los campos no pueden ser llenados sólo con espacios\n"
+            ta.setMensaje("Los campos no pueden ser llenados sólo con espacios\n"
                     + "Si elige un campo texto, debe seleccionar una longitud mayor que cero (0)\n"
                     + "Si elige un campo tipo catálogo, debe seleccionar un catálogo de la lista");
             return mapping.findForward(FAILURE2);
         }
-        
-        
+
+
     }
 }
