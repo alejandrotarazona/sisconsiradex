d235d4172ebde1f97fe044391621cdb2c6f92ffb
jorgedgb@gmail.com <jorgedgb@gmail.com@c823e776-027a-fdf1-4799-f78f589831ee>
2013-06-08 01:50:14 +0000

    Commit por si a las moscas ya que el último lo había hecho sin hacer el checkout. Agregué al webmaster en Registro.sql pa que aparezca en el catalogo de Empleados y facilitarme algo no tener que estarlo agregando pa pruebas cada vez que vuelvo a correr el Schema.

--- SiraDEx/SiraDEx/src/java/Clases/Log.java
+++ SiraDEx/SiraDEx/src/java/Clases/Log.java
@@ -8,8 +8,6 @@ import DBMS.Entity;
 import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
